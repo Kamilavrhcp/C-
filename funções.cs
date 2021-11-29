233aea9180c15ecb8e059a148a5ce8c0011ed2ae
Kamilavrhcp <kamilavrhcp@hotmail.com>
2021-11-29 18:27:00 -0300

    Feat: Funçoes

---
+++ funções.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExibirMsg();
+
+            Console.ReadLine();
+        }
+
+        static void ExibirMsg()
+        {
+            Console.WriteLine("Sistema 1");
+            Console.WriteLine("Sistema 2");
+            Console.WriteLine("Sistema 3");
+        }
+    }
+}
