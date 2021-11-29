e33e7a7e736426f83a38ebbbf7f99d8858a52180
Kamilavrhcp <kamilavrhcp@hotmail.com>
2021-11-29 15:06:57 -0300

    Primeiros passos em C#

---
+++ tipoDeDados.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            var meuTexto = "Hello World";
+            int numeroInteiro = 123;
+            string umTexto = "texto em string";
+            float numDecimal = 192.45f;
+            bool trueAndFalse = true;
+
+            dynamic cor_favorita = "preto";
+            Console.WriteLine(cor_favorita);
+            cor_favorita = 12;
+
+            Console.WriteLine(cor_favorita);
+            Console.ReadLine();
+             */
+            /* -------------------- */
+
+            string nome = "";
+            Console.WriteLine("Escreva seu nome");
+            nome = Console.ReadLine();
+            Console.WriteLine("Seu nome é:");
+            Console.WriteLine(nome);
+
+            Console.ReadLine();
+
+        }
+    }
+}
