74cf424e5b26257f9ca4a68ae53081390646ec7e
Kamilavrhcp <kamilavrhcp@hotmail.com>
2021-11-29 17:39:51 -0300

    Feat: Condicionais

---
+++ condicionais.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (1 > 2);
+            {
+              Console.WriteLine("É verdade!");
+            }
+            else
+            {
+              Console.WriteLine("Não");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
