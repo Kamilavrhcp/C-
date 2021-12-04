091f084c079a9e52450dda9f19e435974c02cc07
Kamilavrhcp <kamilavrhcp@hotmail.com>
2021-12-04 00:13:46 -0300

    Feat: Do While

---
+++ Introdução/doWhile.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 0;
+            while (contador > 0)
+            {
+              Console.WriteLine("Rodando o While!");
+              contador ++;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
