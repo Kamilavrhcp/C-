cd3e0c0c81a47065c1f17026ef1f2d4afd32168a
Kamilavrhcp <kamilavrhcp@hotmail.com>
2021-12-04 01:03:08 -0300

    Feat: for com string decrementando

--- Introdução/forString.cs
+++ Introdução/forString.cs
@@ -13,7 +13,8 @@ namespace ConsoleApplication3
             string[] palavras = {"Subsonica", "Pearl Jam", "SoundGarden", "Red Hot Chili Peppers"};
             Console.log("Melhores bandas do mundo:");
 
-            for(int index = 0; index < palavras.Length; index++)
+            // for(int index = 0; index < palavras.Length; index++) crescente
+            for(int index = palavras[-1]; index >= 0; index--)
             {
               Console.WriteLine(palavras[index]);
 
